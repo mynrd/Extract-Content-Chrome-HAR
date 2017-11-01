ef4020a2714267672b2dca031e510498bf22f78b
Jhon Maynard Bolante <mynrd@live.com>
2017-11-01 10:51:16 +0800

    initial

---
+++ source code/frmMain.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ExtractContentHAR
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnParseData = new System.Windows.Forms.Button();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstUrls = new System.Windows.Forms.ListView();
+            this.status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pattern URL";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Location = new System.Drawing.Point(65, 12);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(717, 21);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(788, 11);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(30, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnParseData
+            // 
+            this.btnParseData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnParseData.Location = new System.Drawing.Point(696, 451);
+            this.btnParseData.Name = "btnParseData";
+            this.btnParseData.Size = new System.Drawing.Size(122, 23);
+            this.btnParseData.TabIndex = 5;
+            this.btnParseData.Text = "Parse Data";
+            this.btnParseData.UseVisualStyleBackColor = true;
+            this.btnParseData.Click += new System.EventHandler(this.btnParseData_Click);
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPattern.Location = new System.Drawing.Point(77, 452);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(613, 21);
+            this.txtPattern.TabIndex = 4;
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseFolder.Location = new System.Drawing.Point(696, 480);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(40, 23);
+            this.btnBrowseFolder.TabIndex = 6;
+            this.btnBrowseFolder.Text = "...";
+            this.btnBrowseFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "File";
+            // 
+            // lstUrls
+            // 
+            this.lstUrls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstUrls.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.status,
+            this.columnHeader1});
+            this.lstUrls.FullRowSelect = true;
+            this.lstUrls.GridLines = true;
+            this.lstUrls.Location = new System.Drawing.Point(12, 39);
+            this.lstUrls.Name = "lstUrls";
+            this.lstUrls.Size = new System.Drawing.Size(802, 402);
+            this.lstUrls.TabIndex = 8;
+            this.lstUrls.UseCompatibleStateImageBehavior = false;
+            this.lstUrls.View = System.Windows.Forms.View.Details;
+            // 
+            // status
+            // 
+            this.status.Text = "Status";
+            this.status.Width = 109;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "URL";
+            this.columnHeader1.Width = 661;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(77, 481);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(613, 21);
+            this.txtPath.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 484);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Save Files";
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExtract.Location = new System.Drawing.Point(742, 480);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(76, 23);
+            this.btnExtract.TabIndex = 11;
+            this.btnExtract.Text = "&Extract";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(829, 515);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lstUrls);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseFolder);
+            this.Controls.Add(this.btnParseData);
+            this.Controls.Add(this.txtPattern);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmMain";
+            this.Text = "Extract Content";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnParseData;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView lstUrls;
+        private System.Windows.Forms.ColumnHeader status;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExtract;
+    }
+}
+
---
+++ source code/frmMain.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Threading;
+
+namespace ExtractContentHAR
+{
+    public partial class frmMain : Form
+    {
+        private List<ItemMessage> Messages;
+        private string Pattern;
+        private System.Windows.Forms.Timer Timer;
+        private bool TimerDispose = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            lstUrls.SelectedIndexChanged += new EventHandler(lstUrls_SelectedIndexChanged);
+            lstUrls.MouseDoubleClick += new MouseEventHandler(lstUrls_MouseDoubleClick);
+            txtFile.KeyDown += new KeyEventHandler(txtFile_KeyDown);
+
+            Messages = new List<ItemMessage>();
+            this.InstanceTimer();
+        }
+
+        private delegate void ProcessDownload(string path);
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Har files (*.har)|*.har|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtFile.Text = ofd.FileName;
+                LoadHARFile(ofd.FileName);
+            }
+        }
+
+        private void btnBrowseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtPath.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            this.TimerDispose = false;
+            this.InstanceTimer();
+            this.Pattern = txtPattern.Text.ToLower().Trim();
+            this.Messages = new List<ItemMessage>();
+            this.Timer.Enabled = true;
+            txtFile_KeyDown(null, new KeyEventArgs(Keys.Enter));
+            txtFile.Enabled = false;
+            btnBrowse.Enabled = false;
+            btnParseData.Enabled = false;
+            btnParseData_Click(null, null);
+            btnExtract.Enabled = false;
+            txtPath.Enabled = false;
+            btnBrowseFolder.Enabled = false;
+
+            Thread th = new Thread(new ParameterizedThreadStart(DownloadFiles));
+            th.Start(txtPath.Text);
+
+        }
+
+        private void btnParseData_Click(object sender, EventArgs e)
+        {
+            btnParseData.Enabled = false;
+            txtPattern.ReadOnly = true;
+            List<ItemMessage> forRemove = new List<ItemMessage>();
+            foreach (var item in this.Messages)
+            {
+                if (!item.URL.ToLower().StartsWith(txtPattern.Text.ToLower()))
+                {
+                    forRemove.Add(item);
+                }
+            }
+            forRemove.ForEach(x =>
+            {
+                lstUrls.Items.Remove(x.ListViewItem);
+                this.Messages.Remove(x);
+            });
+        }
+
+        private void DownloadFiles(object o_path)
+        {
+            ProcessDownload pd = path =>
+            {
+                if (!path.EndsWith(@"\")) path += @"\";
+
+                foreach (var lv in Messages)
+                {
+                    if (lv.URL.ToLower().StartsWith(this.Pattern))
+                    {
+                        Uri uri = new Uri(lv.URL);
+                        string filename = System.IO.Path.GetFileName(uri.LocalPath);
+                        if (string.IsNullOrEmpty(filename)) continue;
+                        string filePath = lv.URL.Replace(txtPattern.Text, "").Replace(@"/", @"\");
+
+                        if (filePath.IndexOf("?") != -1)
+                        {
+                            filePath = filePath.Substring(0, filePath.IndexOf("?"));
+                        }
+
+                        string folder = (path + filePath).Replace(filename, "");
+                        if (!Directory.Exists(folder))
+                        {
+                            try
+                            {
+                                Directory.CreateDirectory(folder);
+                            }
+                            catch (Exception)
+                            {
+                                continue;
+                            }
+                        }
+
+                        if (File.Exists(path + filePath))
+                        {
+                            lv.Status = "File Exist";
+                            continue;
+                        }
+
+                        try
+                        {
+                            lv.Status = "Downloading";
+                            using (WebClient Client = new WebClient())
+                            {
+                                string fileDownloadPath = path + filePath;
+                                fileDownloadPath = fileDownloadPath.Replace(@"\\", @"\");
+                                string url = lv.URL;
+
+                                if (url.IndexOf("?") != -1)
+                                {
+                                    url = url.Substring(0, url.IndexOf("?"));
+                                }
+
+                                if (url.ToLower().Contains(".png") || url.ToLower().Contains(".bmp") || url.ToLower().Contains(".jpg") || url.ToLower().Contains(".jpeg"))
+                                {
+                                    url += "?v=" + DateTime.Now.ToString("MMddyyyyhhmmss");
+                                }
+
+                                Client.DownloadFile(url, fileDownloadPath);
+                            }
+                            lv.Status = "Done";
+                        }
+                        catch (Exception ex)
+                        {
+                            lv.Status = "Error: " + ex.Message;
+                        }
+                    }
+                    else
+                    {
+                        lv.Status = "Excluded: not in pattern.";
+                    }
+                }
+
+                MessageBox.Show("Done", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.TimerDispose = true;
+            };
+
+            pd(o_path.ToString());
+        }
+
+        private void InstanceTimer()
+        {
+            this.Timer = new System.Windows.Forms.Timer();
+            this.Timer.Interval = 1;
+            this.Timer.Tick += new EventHandler(Timer_Tick);
+            this.Timer.Disposed += new EventHandler(Timer_Disposed);
+        }
+
+        private void LoadHARFile(string file)
+        {
+            try
+            {
+                btnParseData.Enabled = true;
+                txtPattern.ReadOnly = false;
+
+                lstUrls.Items.Clear();
+                string content = "";
+                using (StreamReader sr = new StreamReader(file)) { content = sr.ReadToEnd(); }
+                JObject json = JsonConvert.DeserializeObject(content) as JObject;
+                JToken entries = json.First.First["entries"];
+                this.Messages.Clear();
+                foreach (var entry in entries)
+                {
+                    try
+                    {
+                        var im = new ItemMessage() { URL = entry["request"]["url"].ToString(), Status = "--" };
+
+                        if (this.Messages.Any(x => x.URL == im.URL))
+                        {
+                            continue;
+                        }
+                        this.Messages.Add(im);
+                    }
+                    catch { }
+                }
+                foreach (var im in this.Messages.OrderBy(x => x.URL))
+                {
+                    ListViewItem lv = lstUrls.Items.Add(im.Status);
+                    lv.SubItems.Add(im.URL);
+                    im.ListViewItem = lv;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void lstUrls_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                if (lstUrls.FocusedItem != null)
+                {
+                    MessageBox.Show(lstUrls.FocusedItem.Text, "Status Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void lstUrls_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!txtPattern.ReadOnly)
+                txtPattern.Text = lstUrls.FocusedItem.SubItems[1].Text;
+        }
+
+        private void Timer_Disposed(object sender, EventArgs e)
+        {
+            txtFile.Enabled = true;
+            btnBrowse.Enabled = true;
+            btnParseData.Enabled = true;
+            btnExtract.Enabled = true;
+            txtPath.Enabled = true;
+            btnBrowseFolder.Enabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            foreach (var item in this.Messages)
+            {
+                if (item.ListViewItem.Text != item.Status)
+                {
+                    item.ListViewItem.Text = item.Status;
+                }
+            }
+            if (TimerDispose)
+            {
+                this.Timer.Enabled = false;
+                this.Timer.Dispose();
+                this.lstUrls.Items.Clear();
+
+                var items = this.Messages.OrderBy(x => x.Status);
+                foreach (var im in items)
+                {
+                    var lv = lstUrls.Items.Add(im.Status);
+                    lv.SubItems.Add(im.URL);
+                }
+            }
+        }
+
+        private void txtFile_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                LoadHARFile(txtFile.Text);
+            }
+        }
+
+        public class ItemMessage
+        {
+            public ListViewItem ListViewItem { get; set; }
+
+            public string Status { get; set; }
+
+            public string URL { get; set; }
+        }
+    }
+}
